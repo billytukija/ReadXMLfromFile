64e49f5bba3095c79742d83eaa0c2418ce29e990
Billy Tukija Richard <billy.tukija@onlineapp.com.br>
2020-01-27 23:36:30 -0300

    Simple to read a xml from file

---
+++ ReadXMLfromFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml;
+
+namespace ReadXMLfromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                XmlTextReader reader = new XmlTextReader("books.xml");
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element: 
+                            Console.Write("<" + reader.Name);
+                            Console.WriteLine(">");
+                            break;
+                        case XmlNodeType.Text:
+                            Console.WriteLine(reader.Value);
+                            break;
+                        case XmlNodeType.EndElement:
+                            Console.Write("</" + reader.Name);
+                            Console.WriteLine(">");
+                            break;
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
